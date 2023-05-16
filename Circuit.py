854ce1ef931e9423bf6b41d01c88bbf18ffa289d
bsiyoung <siyoung4528@gmail.com>
2023-05-16 19:35:27 +0900

    Circuit.py

---
+++ Circuit.py
@@ -0,0 +1,20 @@
+class Node:
+    pass
+
+class Component:
+    pass
+
+class Element(Component):
+    pass
+
+class Graph(Component):
+    pass
+
+class Resistor(Element):
+    pass
+
+class Inductor(Element):
+    pass
+
+class Capacitor(Element):
+    pass
